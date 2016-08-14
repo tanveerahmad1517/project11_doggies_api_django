cba9b0316e656c9643d375002ce7436e0a777a7a
vasilty <vasilty@gmail.com>
2016-08-14 20:42:19 +0200

    Updated requirements.txt

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_nose',
     'django_cleanup',
     'rest_framework',
     'rest_framework.authtoken',
